c989c719c7ac6f9f996b75d5d7088061ae331f2f
ChamaniS <chamanijks2@gmail.com>
2018-03-29 17:32:47 +0530

    Updating files

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG =
+     {
+    apiKey: "AIzaSyAZMmGGGIGLduLMp3_7bwY-IsplRpFloQw",
+    authDomain: "shoppinglistdb-fd00f.firebaseapp.com",
+    databaseURL: "https://shoppinglistdb-fd00f.firebaseio.com",
+    projectId: "shoppinglistdb-fd00f",
+    storageBucket: "",
+    messagingSenderId: "609716378963"
+  };
